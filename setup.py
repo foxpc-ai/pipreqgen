b70c4c14f731b5d248c7130a8e5cef72d443016c
Muktadir <>
2024-01-10 11:23:26 +0000

    added details

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-# Read the contents of your README file
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
