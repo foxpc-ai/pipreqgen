7cd82fccb01bcbe01ce4682a6bbed53183804e07
Muktadir <79001791+foxpc-ai@users.noreply.github.com>
2024-01-19 15:54:13 +0000

    format

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='pipreqgen',
-    version='0.0.1',
-    author='Muktadir',
-    description='Generates requirement.txt',  
+    name="pipreqgen",
+    version="0.0.1",
+    author="Muktadir",
+    description="Generates requirement.txt",
     long_description=long_description,
-    long_description_content_type='text/markdown',  
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'pipreqgen=pipreqgen.extract_imports:main',
+        "console_scripts": [
+            "pipreqgen=pipreqgen.extract_imports:main",
         ],
     },
     install_requires=[
--- tests/test_extract_imports.py
+++ tests/test_extract_imports.py
@@ -1,48 +1,53 @@
 import unittest
 import os
 import json
-from pipreqgen.extract_imports import extract_imports, extract_imports_from_ipynb, get_versions
+from pipreqgen.extract_imports import (
+    extract_imports,
+    extract_imports_from_ipynb,
+    get_versions,
+)
+
 
 class TestExtractImports(unittest.TestCase):
     def setUp(self):
-        self.filename = 'test.py'
-        with open(self.filename, 'w') as file:
-            file.write('import os\nimport sys\nimport netmiko')
+        self.filename = "test.py"
+        with open(self.filename, "w") as file:
+            file.write("import os\nimport sys\nimport netmiko")
 
-        self.emtyfile = 'empty.py'
-        with open(self.emtyfile, 'w') as efile:
-            efile.write('blank')
+        self.emtyfile = "empty.py"
+        with open(self.emtyfile, "w") as efile:
+            efile.write("blank")
 
-        self.fileipynb = 'test.ipynb'
+        self.fileipynb = "test.ipynb"
         notebook = {
             "cells": [
                 {
                     "cell_type": "code",
-                    "source": ['import numpy\n', 'import pandas as pd\n']
+                    "source": ["import numpy\n", "import pandas as pd\n"],
                 }
             ]
         }
-        with open(self.fileipynb, 'w') as fileipy:
+        with open(self.fileipynb, "w") as fileipy:
             json.dump(notebook, fileipy)
-        
-        self.duplicatefile = 'duplicate.py'
-        with open(self.duplicatefile, 'w') as dupfile:
-            dupfile.write('import os\nimport netmiko\nimport netmiko\nimport flask\nimport sys\nimport sys')
 
+        self.duplicatefile = "duplicate.py"
+        with open(self.duplicatefile, "w") as dupfile:
+            dupfile.write(
+                "import os\nimport netmiko\nimport netmiko\nimport flask\nimport sys\nimport sys"
+            )
 
     def test_extract_imports(self):
-        expected_imports = ['os', 'sys', 'netmiko']
+        expected_imports = ["os", "sys", "netmiko"]
         actual_imports = extract_imports(self.filename)
         self.assertEqual(actual_imports, expected_imports)
 
     def test_extract_imports_nonexistent_file(self):
         with self.assertRaises(FileNotFoundError):
-            extract_imports('nonexistent.py')
-
+            extract_imports("nonexistent.py")
 
     def test_extract_imports_from_ipynb(self):
-        expected_imports = ['numpy', 'pandas']
-        actual_imports = extract_imports_from_ipynb('test.ipynb')
+        expected_imports = ["numpy", "pandas"]
+        actual_imports = extract_imports_from_ipynb("test.ipynb")
         self.assertEqual(actual_imports, expected_imports)
 
     def test_empty_import(self):
@@ -50,27 +55,26 @@ class TestExtractImports(unittest.TestCase):
         actual_imports = extract_imports(self.emtyfile)
         self.assertEqual(actual_imports, expected_imports)
 
-
     def test_extract_imports_from_ipynb_nonexistent_file(self):
         with self.assertRaises(FileNotFoundError):
-            extract_imports_from_ipynb('nonexistent.ipynb')
+            extract_imports_from_ipynb("nonexistent.ipynb")
 
     def test_get_versions(self):
-        modules = ['os', 'sys', 'non_existent_module']
+        modules = ["os", "sys", "non_existent_module"]
         expected_versions = {
-            'os': 'os not found',
-            'sys': 'sys not found',
-            'non_existent_module': 'non_existent_module not found'
+            "os": "os not found",
+            "sys": "sys not found",
+            "non_existent_module": "non_existent_module not found",
         }
-        actual_versions = get_versions(modules,append=True)
+        actual_versions = get_versions(modules, append=True)
         self.assertEqual(actual_versions, expected_versions)
 
     # def test_get_versions_empty_list(self):
     #     with self.assertRaises(ValueError):
     #         get_versions([])
-        
+
     def test_duplicate_imports(self):
-        expected_imports = ['os','netmiko','flask','sys']
+        expected_imports = ["os", "netmiko", "flask", "sys"]
         actual_imports = extract_imports(self.duplicatefile)
         self.assertEqual(actual_imports, expected_imports)
 
@@ -83,5 +87,6 @@ class TestExtractImports(unittest.TestCase):
         except Exception as e:
             print(e)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
